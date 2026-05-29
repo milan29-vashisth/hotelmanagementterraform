46d186b6899ec24fe0d3be956e7907c90129266c
Milan <deepanshu.v7@gmail.com>
2026-05-29 20:49:40 +0530

    added terraform infrastructure

--- main.tf
+++ main.tf
@@ -1,32 +1,33 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "app-test-dev-rg"
+  location = "East US 2"
 }
-
 module "appservice" {
   source = "./modules/appservice"
 
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = azurerm_resource_group.rg.name
   location              = var.location
   app_service_plan_name = var.app_service_plan_name
   app_service_name      = var.app_service_name
+   depends_on = [azurerm_resource_group.rg]
+  
 }
 
 module "acr" {
   source = "./modules/acr"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   acr_name            = var.acr_name
+   depends_on = [azurerm_resource_group.rg]
 }
 
 module "vm" {
-  source = "./modules/vm"
-
-  resource_group_name = var.resource_group_name
+  source              = "./modules/vm"
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-
-  vm_name        = var.vm_name
-  admin_username = var.admin_username
-  admin_password = var.admin_password
+  vm_name             = var.vm_name
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+   depends_on = [azurerm_resource_group.rg]
 }
