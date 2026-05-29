ca12cd0c899f63313d9c3eede2d6e49ac52dd4ed
Milan <deepanshu.v7@gmail.com>
2026-05-29 17:22:02 +0530

    Initial hotel service terraform

--- modules/appservice/main.tf
+++ modules/appservice/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   os_type  = "Linux"
   sku_name = "B1"
@@ -9,19 +9,13 @@ resource "azurerm_service_plan" "plan" {
 
 resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.plan.id
-
-  https_only = true
 
   site_config {
     application_stack {
       node_version = "18-lts"
     }
-  }
-
-  app_settings = {
-    WEBSITE_NODE_DEFAULT_VERSION = "~18"
   }
 }
