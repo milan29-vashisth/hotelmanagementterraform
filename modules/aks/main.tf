f6d9f67e20bb2cd30663763284f581ac3d036a3d
Milan <deepanshu.v7@gmail.com>
2026-06-17 21:10:23 +0530

    added docker code

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dns_prefix
+  oidc_issuer_enabled = true
 
   default_node_pool {
     name       = "system"
