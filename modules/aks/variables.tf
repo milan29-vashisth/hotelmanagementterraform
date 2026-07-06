f7add2fc1c37c0d5b33c874535044a5fbb92fc63
Milan <deepanshu.v7@gmail.com>
2026-07-06 15:56:53 +0530

    Ui update

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,4 +1,4 @@
-# variable "aks_name" {}
-# variable "location" {}
-# variable "resource_group_name" {}
-# variable "dns_prefix" {}
+variable "aks_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "dns_prefix" {}
