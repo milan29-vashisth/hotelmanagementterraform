77d8ea83ce5a229197db8ecc238b97403891451e
Milan <deepanshu.v7@gmail.com>
2026-07-27 14:36:39 +0530

    update

--- main.tf
+++ main.tf
@@ -2,39 +2,39 @@ resource "azurerm_resource_group" "rg" {
   name     = "app-test-dev-rg"
   location = "East US 2"
 }
-module "appservice" {
-  source = "./modules/appservice"
+# module "appservice" {
+#   source = "./modules/appservice"
 
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = var.location
-  app_service_plan_name = var.app_service_plan_name
-  app_service_name      = var.app_service_name
-   depends_on = [azurerm_resource_group.rg]
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   location              = var.location
+#   app_service_plan_name = var.app_service_plan_name
+#   app_service_name      = var.app_service_name
+#    depends_on = [azurerm_resource_group.rg]
   
-}
+# }
 
-module "acr" {
-  source = "./modules/acr"
+# module "acr" {
+#   source = "./modules/acr"
 
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  acr_name            = var.acr_name
-   depends_on = [azurerm_resource_group.rg]
-}
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = var.location
+#   acr_name            = var.acr_name
+#    depends_on = [azurerm_resource_group.rg]
+# }
 
-module "vm" {
-  source              = "./modules/vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  vm_name             = var.vm_name
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-   depends_on = [azurerm_resource_group.rg]
-}
-module "aks" {
-  source              = "./modules/aks"
-  aks_name            = var.aks_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "hotelaks"
-}
+# module "vm" {
+#   source              = "./modules/vm"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = var.location
+#   vm_name             = var.vm_name
+#   admin_username      = var.admin_username
+#   admin_password      = var.admin_password
+#    depends_on = [azurerm_resource_group.rg]
+# }
+# module "aks" {
+#   source              = "./modules/aks"
+#   aks_name            = var.aks_name
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   dns_prefix          = "hotelaks"
+# }
